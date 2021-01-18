bc7181b10b98384f0317e8f3de705d2e8306b73e
Pedro Dias <59458685+pedromndias@users.noreply.github.com>
2021-01-18 14:48:06 +0100

    Updated the counter to be before the text edit on the document

--- Upvoting app/app.js
+++ Upvoting app/app.js
@@ -1,11 +1,11 @@
 let counter = 3;
 
 function upvote() {
-    document.getElementById("votes").innerHTML = counter + " votes";
     counter++;
+    document.getElementById("votes").innerHTML = counter + " votes";
 }
 
 function downvote() {
-    document.getElementById("votes").innerHTML = counter + " votes";
     counter--;
+    document.getElementById("votes").innerHTML = counter + " votes";
 }
